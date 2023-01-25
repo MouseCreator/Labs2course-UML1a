27b15cad41517d2ae37429e49788a1c831944464
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-01-25 03:15:53 +0200

    Created a few tests for Folder List: removing, converting

---
+++ src/test/java/com/example/lab23a/model/FolderListTest.java
@@ -0,0 +1,44 @@
+package com.example.lab23a.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FolderListTest {
+
+    private FolderList list;
+
+    Folder[] arrayTestFolders = new Folder[4];
+    @BeforeEach
+    public void fillList() {
+        list = new FolderList();
+
+        arrayTestFolders[0] = new Folder(1, "C");
+        arrayTestFolders[1] = new Folder(3, "B");
+        arrayTestFolders[2] = new Folder(4, "A");
+        arrayTestFolders[3] = new Folder(5, "D");
+
+        for (Folder toAdd : arrayTestFolders)
+            list.add(toAdd);
+    }
+    @Test
+    void testRemoveFolder() {
+        int initialSize = list.size();
+
+        list.remove(arrayTestFolders[0]);
+        list.remove(arrayTestFolders[0]);
+
+        list.remove(0);
+        assertEquals(list.size(), initialSize - 2);
+    }
+
+    @Test
+    void testToArray() {
+        ArrayList<Folder> arrayFolders = list.asArrayList();
+        assertTrue(Arrays.deepEquals(arrayFolders.toArray(), arrayTestFolders));
+    }
+}
