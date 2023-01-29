d3dce5b0434eccf1e024a0dc4874e7c9a5efd23b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-01-29 19:34:17 +0200

    Added folders documentation

--- src/main/java/com/example/lab23a/model/FolderList.java
+++ src/main/java/com/example/lab23a/model/FolderList.java
@@ -6,35 +6,77 @@ import java.util.Comparator;
 public class FolderList {
 	private final ArrayList<Folder> folders;
 	private final Comparator<Folder> comparatorByName = Comparator.comparing(Folder::getName);
+
+	/**
+	 * creates empty list of folders
+	 */
 	public FolderList() {
 		folders = new ArrayList<>();
 	}
+
+	/**
+	 *
+	 * @param other - list of folders to copy
+	 */
 	public FolderList(FolderList other) {
 		folders = new ArrayList<>(other.folders);
 	}
+
+	/**
+	 *
+	 * @param folder - folder to add to the list
+	 */
 	public void add(Folder folder) {
 		folders.add(folder);
 	}
+
+	/**
+	 *
+	 * @param index - number of folder in the list
+	 * @return specified folder
+	 */
 	public Folder get(int index) {
 		return folders.get(index);
 	}
+
+	/**
+	 *
+	 * @param folder - folder to remove. If it is not in the list, nothing will be deleted
+	 */
 	public void remove(Folder folder) {
 		folders.remove(folder);
 	}
-	public Folder remove(int index) {
-		return folders.remove(index);
+
+	/**
+	 *
+	 * @param index - number of folder in the list to remove
+	 */
+	public void remove(int index) {
+		folders.remove(index);
 	}
+
+	/**
+	 *
+	 * @return number of folders in the list
+	 */
 	public int size() {
 		return folders.size();
 	}
-	public boolean isEmpty() {
-		return folders.isEmpty();
-	}
+
+	/**
+	 *
+	 * @return list with the same folders, sorted in alphabetic order
+	 */
 	public FolderList sortByName() {
 		FolderList result = new FolderList(this);
 		result.folders.sort(comparatorByName);
 		return result;
 	}
+
+	/**
+	 *
+	 * @return array list, containing all folders in this list
+	 */
 	public ArrayList<Folder> asArrayList() {
 		return folders;
 	}
