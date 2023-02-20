e154b20567d6a501521214f62c4576ea9b01dc0e
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-02-20 03:35:58 +0200

    Implemented close operation

--- src/main/java/com/example/lab23a/Server/ClientHandler.java
+++ src/main/java/com/example/lab23a/Server/ClientHandler.java
@@ -1,28 +1,44 @@
 package com.example.lab23a.Server;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable{
 
-    private final BufferedWriter bufferedWriter;
-    private final BufferedWriter bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
     private Socket socket;
     
     public ClientHandler(Socket socket) {
         this.socket = socket;
         try {
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            this.bufferedReader = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            close();
         }
     }
 
     @Override
     public void run() {
+        String message;
+        while (socket.isConnected()) {
+            try {
+                message = bufferedReader.readLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public void close() {
+        try {
+            socket.close();
+            bufferedWriter.close();
+            bufferedReader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
