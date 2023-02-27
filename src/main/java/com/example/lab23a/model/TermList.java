d9a841670eeb2852fec4158fde548ae58ffd75fb
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-02-27 10:12:29 +0200

    Removed abstract term list

--- src/main/java/com/example/lab23a/model/TermList.java
+++ src/main/java/com/example/lab23a/model/TermList.java
@@ -1,9 +1,43 @@
 package com.example.lab23a.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class TermList extends AbstractTermList {
+public class TermList implements Iterable<StudyTerm>{
+    protected ArrayList<StudyTerm> terms;
+
+    public Iterator<StudyTerm> iterator() {
+        return new Iterator<>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size();
+            }
+
+            @Override
+            public StudyTerm next() {
+                return terms.get(currentIndex++);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+    public int size() {
+        return terms.size();
+    }
+
+    public void add(StudyTerm term) {
+        terms.add(term);
+    }
+
+    public StudyTerm getByIndex(int index) {
+        return terms.get(index);
+    }
 
     public TermList() {
         terms = new ArrayList<>();
