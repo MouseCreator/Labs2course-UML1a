b4858cac6a38b80e1cca0b0b9a3e0f14e88739cd
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-03-04 01:19:33 +0200

    Added controller for pop-up documentation

--- src/main/java/com/example/lab23a/PopupController.java
+++ src/main/java/com/example/lab23a/PopupController.java
@@ -4,30 +4,53 @@ import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * Class for pop-up windows with two options
+ */
 public class PopupController {
 	@FXML
     private Text messageLabel;
-
-     
-    
+    /**
+     * Action to do when user accepted the proposition
+     */
     private Runnable acceptCommand;
-    
+    /**
+     * Action to do when user declined the proposition
+     */
     private Runnable declineCommand;
-    
+
+    /**
+     *
+     * @param message - text to display, when the pop-up is shown
+     */
     public void setMessage(String message) {
     	this.messageLabel.setText(message);
     }
+
+    /**
+     *
+     * @param method - action to do, when user accepted the proposition
+     */
     public void setOnAccept(Runnable method) {
     	this.acceptCommand = method;
     }
+    /**
+     *
+     * @param method - action to do, when user declined the proposition
+     */
     public void setOnDecline(Runnable method) {
     	this.declineCommand = method;
     }
+    /**
+     * Perform the accept action
+     */
     public void onAccepted() {
     	acceptCommand.run();
     	close();
     }
-    
+    /**
+     * Perform the decline action
+     */
     public void onDecline() {
     	declineCommand.run();
     	close();
